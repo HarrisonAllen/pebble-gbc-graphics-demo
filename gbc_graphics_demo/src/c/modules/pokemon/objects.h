982c02f92a1d52628e55799e86100e8f25cefd32
Harrison Allen <harrisonallen555@gmail.com>
2021-04-23 16:45:23 -0400

    added more objects to the map, changed how they work

--- gbc_graphics_demo/src/c/modules/pokemon/objects.h
+++ gbc_graphics_demo/src/c/modules/pokemon/objects.h
@@ -2,8 +2,19 @@
 
 #include <pebble.h>
 
-const uint16_t dialogue_objects[][3] = {
-    // {block x, block y, dialogue_id}
-    {51, 11, 3}
+typedef enum {
+    PO_NONE,
+    PO_SIGN,
+    PO_CUT_TREE,
+    PO_DOOR
+} PokemonObjectTypes;
+
+const int16_t pokemon_objects[][4] = {
+    // {block x, block y, type, data}
+    {51, 11, PO_SIGN, 0}, // Sign #1 (data: dialogue)
+    {5, 9, PO_SIGN, 1}, // Sign #2 (data: dialogue)
+    {21, 15, PO_CUT_TREE, 2}, // Cut tree #1 (data: dialogue)
+    {30, 13, PO_CUT_TREE, 2}, // Cut tree #2 (data: dialogue)
+    {27, 5, PO_DOOR, 3}, // Door (data: dialogue) (in full example, could be warp data)
 };
 

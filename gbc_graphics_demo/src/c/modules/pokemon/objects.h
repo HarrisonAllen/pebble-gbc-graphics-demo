1f9731c90ac807e9faf2e3dc4c399c476eb9a825
Harrison Allen <harrisonallen555@gmail.com>
2021-07-01 06:19:27 -0400

    adding some old changes that are just sitting there

--- gbc_graphics_demo/src/c/modules/pokemon/objects.h
+++ gbc_graphics_demo/src/c/modules/pokemon/objects.h
@@ -9,12 +9,12 @@ typedef enum {
     PO_DOOR
 } PokemonObjectTypes;
 
-const int16_t pokemon_objects[][4] = {
-    // {block x, block y, type, data}
-    {51, 11, PO_SIGN, 0}, // Sign #1 (data: dialogue)
-    {5, 9, PO_SIGN, 1}, // Sign #2 (data: dialogue)
-    {21, 15, PO_CUT_TREE, 2}, // Cut tree #1 (data: dialogue)
-    {30, 13, PO_CUT_TREE, 2}, // Cut tree #2 (data: dialogue)
-    {27, 5, PO_DOOR, 3}, // Door (data: dialogue) (in full example, could be warp data)
+const int16_t pokemon_objects[][5] = {
+    // {route, block x, block y, type, data}
+    {0, 51, 11, PO_SIGN, 0}, // Sign #1 (data: dialogue)
+    {0, 5, 9, PO_SIGN, 1}, // Sign #2 (data: dialogue)
+    {0, 21, 15, PO_CUT_TREE, 2}, // Cut tree #1 (data: dialogue)
+    {0, 30, 13, PO_CUT_TREE, 2}, // Cut tree #2 (data: dialogue)
+    {0, 27, 5, PO_DOOR, 3}, // Door (data: dialogue) (in full example, could be warp data)
 };
 

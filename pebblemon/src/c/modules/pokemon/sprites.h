9a614bde0067b5c74377337037b6642db5fe7598
Harrison Allen <harrisonallen555@gmail.com>
2021-07-01 06:25:04 -0400

    added b&w sprites and removed old code

--- pebblemon/src/c/modules/pokemon/sprites.h
+++ pebblemon/src/c/modules/pokemon/sprites.h
@@ -13,19 +13,9 @@
 #define POKEMON_TRAINER_WALK_LEFT 5
 #define POKEMON_TRAINER_WALK_RIGHT 5
 
-// There are like a million more trainer sprites in the file, but this will do for demo
-#define POKEMON_SPRITE_CHRIS 0
-#define POKEMON_SPRITE_CHRIS_BIKE 6
-#define POKEMON_SPRITE_KRIS 12
-#define POKEMON_SPRITE_KRIS_BIKE 18
-#define POKEMON_SPRITE_RIVAL 31
-#define POKEMON_SPRITE_OAK 37
-#define POKEMON_SPRITE_RED 43
-#define POKEMON_SPRITE_BLUE 49
-
-#define POKEMON_SPRITE_PARTY 428 // Yeah I'm not using them, but they're there!
-#define POKEMON_SPRITE_EMOTICONS 504 // Okay not using these either, sue me
-#define POKEMON_SPRITE_OVERWORLD_POKEMON 512 // Sure fine I know I'm wasting storage space
+#define POKEMON_SPRITE_PARTY 428
+#define POKEMON_SPRITE_EMOTICONS 504
+#define POKEMON_SPRITE_OVERWORLD_POKEMON 512 
 #define POKEMON_SPRITE_ITEMS 563
 #define POKEMON_SPRITE_CUT_TREE 567
 #define POKEMON_SPRITE_ITEM 566
@@ -68,6 +58,7 @@ uint16_t pokemon_trainer_sprites[] = {
 };
 
 uint8_t pokemon_trainer_palettes[][4] = {
+#if defined(PBL_COLOR)
     {0b11111111, 0b11111001, 0b11110000, 0b11000000},    // Chris
     {0b11111111, 0b11111001, 0b11010111, 0b11000000},    // Kris
     {0b11111111, 0b11111001, 0b11110001, 0b11000000},    // Rival
@@ -90,33 +81,28 @@ uint8_t pokemon_trainer_palettes[][4] = {
     {0b11111111, 0b11111111, 0b11111011, 0b11000000},    // Clefairy
     {0b11111111, 0b11111110, 0b11110100, 0b11000000},    // Dragon
     {0b11111111, 0b11111101, 0b11110000, 0b11000000},    // Surfing Pikachu
-};
-
-uint8_t pokemon_trainer_sprite_palettes[][4] = {
-#if defined(PBL_COLOR)
-    {0b11111111, 0b11111001, 0b11110000, 0b11000000},
-    {0b11111111, 0b11111001, 0b11010111, 0b11000000},
-    {0b11111111, 0b11111001, 0b11001000, 0b11000000},
-    {0b11111111, 0b11111001, 0b11010100, 0b11000000},
-    {0b11111111, 0b11111001, 0b11110101, 0b11000000},
-    {0b11111111, 0b11111111, 0b11010101, 0b11000000},
 #else
-    {1, 1, 0, 0},
-    {1, 1, 0, 0},
-    {1, 1, 0, 0},
-    {1, 1, 0, 0},
-    {1, 1, 0, 0},
-    {1, 1, 0, 0},
+    {1, 1, 0, 0},    // Chris
+    {1, 1, 0, 0},    // Kris
+    {1, 1, 0, 0},    // Rival
+    {1, 1, 0, 0},    // Prof. Elm
+    {1, 1, 0, 0},    // Prof. Oak
+    {1, 1, 0, 0},    // Nurse
+    {1, 1, 0, 0},    // Team Rocket Grunt (Male)
+    {1, 1, 0, 0},    // Team Rocket Grunt (Female)
+    {1, 1, 0, 0},    // Mom
+    {1, 1, 0, 0},    // Jasmine
+    {1, 1, 0, 0},    // Misty
+    {1, 1, 0, 0},    // Brock
+    {1, 1, 0, 0},    // Bruno
+    {1, 1, 0, 0},    // Old Man
+    {1, 1, 0, 0},    // Old Woman
+    {1, 1, 0, 0},    // Kimono Girl
+    {1, 1, 0, 0},    // Gary (Blue)
+    {1, 1, 0, 0},    // Clair
+    {1, 1, 0, 0},    // Lance
+    {1, 1, 0, 0},    // Clefairy
+    {1, 1, 0, 0},    // Dragon
+    {1, 1, 0, 0},    // Surfing Pikachu
 #endif
-};
-
-const uint8_t pokemon_trainer_sheet_offsets[] = {
-    POKEMON_SPRITE_CHRIS,
-    POKEMON_SPRITE_CHRIS_BIKE,
-    POKEMON_SPRITE_KRIS,
-    POKEMON_SPRITE_KRIS_BIKE,
-    POKEMON_SPRITE_RIVAL,
-    POKEMON_SPRITE_OAK,
-    POKEMON_SPRITE_RED,
-    POKEMON_SPRITE_BLUE,
 };

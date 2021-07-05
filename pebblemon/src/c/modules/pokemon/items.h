fded1851a3e878d2f6b96a0b0f76c46f653bd22e
Harrison Allen <harrisonallen555@gmail.com>
2021-07-05 19:59:04 -0400

    fixed item locations, finalized data structure, fixed GET/HAS/SET_ITEM

--- pebblemon/src/c/modules/pokemon/items.h
+++ pebblemon/src/c/modules/pokemon/items.h
@@ -11,18 +11,22 @@
 #define ITEM_ID_PROTEIN 6       // Protein (Raises attack stat by 25% each battle) - Route 1, top right
 #define ITEM_ID_IRON 7          // Iron (Raises defense stat by 25% each battle) - Cave, on stairs
 
-#define GET_ITEM_FLAG(id) 1 << (id)
-#define HAS_ITEM(data, id) (data) & (1 << (id)) // Use to check if an item is obtained
-#define SET_ITEM(data, id) (data) | (1 << (id)) // Set data equal to this to set an item as obtained
+#define ITEM_SPRITE_HIDDEN 583
+#define ITEM_SPRITE_POKEBALL 571
+
+
+#define GET_ITEM_FLAG(id) (1 << (id))
+#define HAS_ITEM(data, id) ((data) & (1 << (id))) // Use to check if an item is obtained
+#define SET_ITEM(data, id) ((data) | (1 << (id))) // Set data equal to this to set an item as obtained
 
 const int16_t items[][6] = {
-    // route, block_x, block_y, item_id, sprite, hide
-    {1, 19, 18, 0, 0, false}, // Running Shoes (double move speed) - Forest, top half
-    {1, 29, 26, 0, 0, false}, // Cut (enables cutting of trees) - Forest, bottom half
-    {2, 37, 15, 0, 0, false}, // Lucky Egg (double exp) - National Park, top right
-    {0, 12, 16, 3, 0, false}, // Berry (heals 10HP when below half, one time use(?)) - Berry tree route 1, can come back for more(?)
-    {2, 32, 42, 0, 0, false}, // Leftovers (restore a 1/16 HP each turn) - Far right trash can National Park
-    {0, 19, 23, 0, 0, false}, // Focus Band (12% chance to prevent fainting) - Cave, in path 
-    {0, 48, 16, 6, 0, false}, // Protein (Raises attack stat by 25% each battle) - Route 1, top right
-    {0, 7, 21, 0, 0, false}, // Iron (Raises defense stat by 25% each battle) - Cave, on stairs
+    // route, block_x, block_y, item_id, sprite
+    {1, 19, 18, ITEM_ID_RUNNING_SHOES, ITEM_SPRITE_POKEBALL}, // Running Shoes (double move speed) - Forest, top half
+    {1, 29, 26, ITEM_ID_CUT, ITEM_SPRITE_POKEBALL}, // Cut (enables cutting of trees) - Forest, bottom half
+    {2, 37, 15, ITEM_ID_LUCKY_EGG, ITEM_SPRITE_POKEBALL}, // Lucky Egg (double exp) - National Park, top right
+    {3, 12, 6, ITEM_ID_BERRY, ITEM_SPRITE_POKEBALL}, // Berry (heals 10HP when below half, one time use(?)) - Berry tree route 1, can come back for more(?)
+    {2, 32, 42, ITEM_ID_LEFTOVERS, ITEM_SPRITE_HIDDEN}, // Leftovers (restore a 1/16 HP each turn) - Far right trash can National Park
+    {0, 19, 23, ITEM_ID_FOCUS_BAND, ITEM_SPRITE_POKEBALL}, // Focus Band (12% chance to prevent fainting) - Cave, in path 
+    {3, 48, 6, ITEM_ID_PROTEIN, ITEM_SPRITE_POKEBALL}, // Protein (Raises attack stat by 25% each battle) - Route 1, top right
+    {0, 7, 21, ITEM_ID_IRON, ITEM_SPRITE_POKEBALL}, // Iron (Raises defense stat by 25% each battle) - Cave, on stairs
 };

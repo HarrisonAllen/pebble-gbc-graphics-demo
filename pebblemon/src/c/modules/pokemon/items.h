aad58abb9039e06aef0548aaf7e9ff15e30997d6
Harrison Allen <harrisonallen555@gmail.com>
2021-07-06 11:00:35 -0400

    redid data structure, added item names and infos

--- pebblemon/src/c/modules/pokemon/items.h
+++ pebblemon/src/c/modules/pokemon/items.h
@@ -2,6 +2,7 @@
 
 #include <pebble.h>
 
+#define NUM_ITEMS 8
 #define ITEM_ID_RUNNING_SHOES 0 // Running Shoes (double move speed) - Forest, top half
 #define ITEM_ID_CUT 1           // Cut (enables cutting of trees) - Forest, bottom half
 #define ITEM_ID_LUCKY_EGG 2     // Lucky Egg (double exp) - National Park, top right
@@ -11,22 +12,43 @@
 #define ITEM_ID_PROTEIN 6       // Protein (Raises attack stat by 25% each battle) - Route 1, top right
 #define ITEM_ID_IRON 7          // Iron (Raises defense stat by 25% each battle) - Cave, on stairs
 
-#define ITEM_SPRITE_HIDDEN 583
-#define ITEM_SPRITE_POKEBALL 571
-
+// #define ITEM_SPRITE_HIDDEN 583
+#define ITEM_SPRITE_POKEBALL 566
 
 #define GET_ITEM_FLAG(id) (1 << (id))
 #define HAS_ITEM(data, id) ((data) & (1 << (id))) // Use to check if an item is obtained
 #define SET_ITEM(data, id) ((data) | (1 << (id))) // Set data equal to this to set an item as obtained
 
+// route, block_x, block_y, item_id, hidden
 const int16_t items[][6] = {
-    // route, block_x, block_y, item_id, sprite
-    {1, 19, 18, ITEM_ID_RUNNING_SHOES, ITEM_SPRITE_POKEBALL}, // Running Shoes (double move speed) - Forest, top half
-    {1, 29, 26, ITEM_ID_CUT, ITEM_SPRITE_POKEBALL}, // Cut (enables cutting of trees) - Forest, bottom half
-    {2, 37, 15, ITEM_ID_LUCKY_EGG, ITEM_SPRITE_POKEBALL}, // Lucky Egg (double exp) - National Park, top right
-    {3, 12, 6, ITEM_ID_BERRY, ITEM_SPRITE_POKEBALL}, // Berry (heals 10HP when below half, one time use(?)) - Berry tree route 1, can come back for more(?)
-    {2, 32, 42, ITEM_ID_LEFTOVERS, ITEM_SPRITE_HIDDEN}, // Leftovers (restore a 1/16 HP each turn) - Far right trash can National Park
-    {0, 19, 23, ITEM_ID_FOCUS_BAND, ITEM_SPRITE_POKEBALL}, // Focus Band (12% chance to prevent fainting) - Cave, in path 
-    {3, 48, 6, ITEM_ID_PROTEIN, ITEM_SPRITE_POKEBALL}, // Protein (Raises attack stat by 25% each battle) - Route 1, top right
-    {0, 7, 21, ITEM_ID_IRON, ITEM_SPRITE_POKEBALL}, // Iron (Raises defense stat by 25% each battle) - Cave, on stairs
+    {1, 19, 18, ITEM_ID_RUNNING_SHOES, false}, // Running Shoes (double move speed) - Forest, top half
+    {1, 29, 26, ITEM_ID_CUT, false}, // Cut (enables cutting of trees) - Forest, bottom half
+    {2, 37, 15, ITEM_ID_LUCKY_EGG, false}, // Lucky Egg (double exp) - National Park, top right
+    {3, 12, 6, ITEM_ID_BERRY, false}, // Berry (heals 10HP when below half, one time use(?)) - Berry tree route 1, can come back for more(?)
+    {2, 32, 42, ITEM_ID_LEFTOVERS, true}, // Leftovers (restore a 1/16 HP each turn) - Far right trash can National Park
+    {0, 19, 23, ITEM_ID_FOCUS_BAND, false}, // Focus Band (12% chance to prevent fainting) - Cave, in path 
+    {3, 48, 6, ITEM_ID_PROTEIN, false}, // Protein (Raises attack stat by 25% each battle) - Route 1, top right
+    {0, 7, 21, ITEM_ID_IRON, false}, // Iron (Raises defense stat by 25% each battle) - Cave, on stairs
+};
+
+char *item_names[] = {
+    "RUNNING SHOES ",
+    "HM01 CUT      ",
+    "LUCKY EGG     ",
+    "BERRY         ",
+    "LEFTOVERS     ",
+    "FOCUS BAND    ",
+    "PROTEIN       ",
+    "IRON          "
+};
+
+char *item_infos[] = {
+    "      Speed up",
+    "     Cut trees",
+    "        EXP up",
+    "   Low HP heal",
+    "     Turn heal",
+    "    Faint less",
+    "     Attack up",
+    "    Defense up"
 };

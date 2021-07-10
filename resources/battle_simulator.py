aea6ab5b7fa4ea2165ec3fdfdf445cd832b25cb1
Harrison Allen <harrisonallen555@gmail.com>
2021-07-09 21:25:39 -0400

    ran some more simulations

--- resources/battle_simulator.py
+++ resources/battle_simulator.py
@@ -10,7 +10,7 @@ def simulate_battles(show_battles=False):
     num_battles = 0
     prev_battles = -1
     while level < 100:
-        exp_gain = calculate_exp_gain(randint(level - 4, level + 5), level > 30)
+        exp_gain = calculate_exp_gain(randint(level - 4, level + 5), level > 20)
         next_level = (level + 1)**3
         while exp_gain > 0:
             exp_gain -= 1
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     num_simulations = 100
     total = 0
     for i in range(100):
-        battles = simulate_battles(True)
+        battles = simulate_battles(False)
         print(f'{battles} battles')
         total += battles
     print(f'On average, takes {total // num_simulations} battles')
